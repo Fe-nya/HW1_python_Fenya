ca3dac1090f72689e072eb89df475f5df4c62153
fenya1 <verymagic12@gmail.com>
2023-03-18 15:16:13 +0300

    Corrected 6 task

--- hw1_task6.py
+++ hw1_task6.py
@@ -1,4 +1,3 @@
-import math
 a = int(input())
 b = int(input())
 c = int(input())
@@ -6,13 +5,13 @@ if a != 1:
     b = b//a
     c = c//a
     a = 1
-x1 = (((-b) + (math.sqrt((b ** 2 - 4 * a * c)))) / (2 * a))
-x2 = (((-b) - (math.sqrt((b ** 2 - 4 * a * c)))) / (2 * a))
-if x1 != 0 and x2 != 0:
-    print(x1, x2)
-elif x1 == 0 and x2 != 0:
-    print(x2)
-elif x1 != 0 and x2 == 0:
+i = (b ** 2 - 4 * a * c)
+x1 = (((-b) + (i ** 0.5)) / (2 * a))
+x2 = (((-b) - (i ** 0.5)) / (2 * a))
+
+if x1 == x2:
     print(x1)
 elif x1 == 0 and x2 == 0:
     print()
+else:
+    print(x1, x2)
