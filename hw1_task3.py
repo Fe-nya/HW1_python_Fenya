93d1f95f3e18af580b9c72d85ab1627f6df7ad4a
fenya1 <verymagic12@gmail.com>
2023-03-20 19:52:33 +0300

    Corrected 3 task

--- hw1_task3.py
+++ hw1_task3.py
@@ -2,25 +2,34 @@ n = int(input())
 one = n % 10
 two = n // 10
 three = n // 60
+
 if n <= 8:
     print(n, 0, 0)
+
+
 elif 8 < n <= 30:
-    if n >= 8:
+    if one >= 8:
         print(0, two + 1, 0)
     else:
         print(one, two, 0)
+
+
 elif n > 30:
-    i = n - three * 60
     if n <= 60:
-        print(0, 0, 1)
+        if one <= 4:
+            print(one, two, 0)
+        else:
+            print(0, 0, 1)
+
     else:
+        i = n - three * 60
         if i >= 31:
-            if one >= 8:
-                print(0, 1, three + 1)
+            if one <= 4:
+                print(one, i // 10, three)
             else:
-                print(one, 0, three + 1)
+                print(0, 0, three + 1)
         else:
             if one >= 8:
-                print(0, i//10 + 1, three)
+                print(0, i // 10 + 1, three)
             else:
-                print(one, i//10, three)
+                print(one, i // 10, three)
