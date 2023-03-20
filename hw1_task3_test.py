be45d5af1840a130560065e770a5478b56d9df42
fenya1 <verymagic12@gmail.com>
2023-03-20 18:53:44 +0300

    Created test for 3 task

---
+++ hw1_task3_test.py
@@ -0,0 +1,9 @@
+price = input().split()
+one = int(price[0])
+two = int(price[1])
+three = int(price[2])
+c1 = one * 15
+c2 = two * 125
+c3 = three * 440
+print(c1, c2, c3)
+print('Itogo: ', c1 + c2 + c3)
