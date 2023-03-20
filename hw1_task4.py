283eff605a48721f91cbdd6d7c4e59b0b87e714e
fenya1 <verymagic12@gmail.com>
2023-03-20 19:06:20 +0300

    Completed 4 task

---
+++ hw1_task4.py
@@ -0,0 +1,26 @@
+x = int(input())
+ten = x // 10
+one = x % 10
+
+if ten == 10:
+    result = 'C'
+elif ten == 9:
+    result = 'XC'
+elif ten == 4:
+    result = 'XL'
+elif ten >= 5:
+    result = 'L' + (ten - 5) * 'X'
+else:
+    result = ten * 'X'
+
+
+if one == 9:
+    result += 'IX'
+elif one == 4:
+    result += 'IV'
+elif one >= 5:
+    result += 'V' + (one - 5) * 'I'
+else:
+    result += one * 'I'
+print(result)
+
